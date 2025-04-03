ac790ace4d253e9e80907c0fd62c52e75fa2467a
7Silva <zaronymogamer@gmail.com>
2025-04-02 21:15:08 -0300

    ✨ feat(ui): add project header variant to card header
    - add new "project" type to card header component
    - create projectHeader component to display client, company, and year
    - update cardHeader to render titleHeader or projectHeader based on props.type

--- src/components/ui/cards/layout/header.tsx
+++ src/components/ui/cards/layout/header.tsx
@@ -1,9 +1,33 @@
-interface CardHeaderProps {
+type CardHeaderBaseProps = {
   title: string
+}
+
+type TitleHeaderProps = CardHeaderBaseProps & {
+  type: 'title'
   children?: React.ReactNode
 }
 
-export default function CardHeader({ title, children }: CardHeaderProps) {
+type ProjectHeaderProps = {
+  type: 'project'
+  clientName: string
+  companyName: string
+  projectYear: number
+}
+
+type CardHeaderProps = TitleHeaderProps | ProjectHeaderProps
+
+export default function CardHeader(props: CardHeaderProps) {
+  switch (props.type) {
+    case 'title':
+      return <TitleHeader {...props} />
+    case 'project':
+      return <ProjectHeader {...props} />
+    default:
+      return null
+  }
+}
+
+function TitleHeader({ title, children }: TitleHeaderProps) {
   return (
     <div className="flex w-full items-center justify-between">
       <div className="flex items-center gap-2">
@@ -14,6 +38,27 @@ export default function CardHeader({ title, children }: CardHeaderProps) {
       </div>
 
       {children}
+    </div>
+  )
+}
+
+function ProjectHeader({
+  clientName,
+  companyName,
+  projectYear,
+}: ProjectHeaderProps) {
+  return (
+    <div className="bg-border flex w-full items-center gap-12 rounded-xl p-4">
+      <div className="space-y-1 text-white/60">
+        <p>Client</p>
+        <p>Company</p>
+        <p>Year</p>
+      </div>
+      <div className="space-y-1 font-medium">
+        <p>{clientName}</p>
+        <p>{companyName}</p>
+        <p>{projectYear}</p>
+      </div>
     </div>
   )
 }
